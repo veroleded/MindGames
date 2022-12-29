f808ca8d7cb28adbab943f01cc6fe2224bb77d17
veroleded <timur1776@gmail.com>
2022-12-29 22:24:10 +0300

    update cli.js

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@ import readLineSync from 'readline-sync';
 
 export default function greeting() {
   console.log('Welcome to the Brain Games!');
-  let userName = readLineSync.question('May I have your name?');
+  let userName = readLineSync.question('May I have your name? ');
   while (1) {
     if (userName === '') {
       userName = readLineSync.question('There"s no game without a name, so say your name! ');
